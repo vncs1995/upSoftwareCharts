7cd2bfd5007fcf9d09ddb39d2ef8527fde96b9d4
Viniciu Viana dos Santos <vinicius.viana@accenture.com>
2018-06-23 01:55:58 -0300

    [WIP] - Tela de dados apenas mostrando o layout, falta os modais para aparecerem os demais gráficos de acordo com a sessão

--- src/pages/dados/dados.ts
+++ src/pages/dados/dados.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+import { ModalProdutosPage } from '../modal-produtos/modal-produtos';
+import { ModalFabricantesPage } from '../modal-fabricantes/modal-fabricantes';
+import { ModalClientesPage } from '../modal-clientes/modal-clientes';
 
 @Component({
   selector: 'page-dados',
@@ -7,10 +11,31 @@ import { NavController } from 'ionic-angular';
 })
 export class DadosPage {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) { }
 
-  public doughnutChartLabels:string[] = ['Me', 'Myself', 'and I'];
-  public doughnutChartData:number[] = [123, 456, 789];
-  public doughnutChartType:string = 'doughnut';
+  public doughnutChartLabels: string[] = ['Me', 'Myself', 'and I'];
+  public doughnutChartData: number[] = [123, 456, 789];
+  public doughnutChartType: string = 'pie';
+
+  public pieChartLabels: string[] = ['positive', 'negative'];
+  public pieChartData: number[] = [82.69, 17.31];
+  public pieChartType: string = 'pie';
+
+  openModal(modalToOpen) {
+    let modal;
+    switch (modalToOpen) {
+      case 'produtos':
+      modal = this.modalCtrl.create(ModalProdutosPage);
+        break;
+      case 'fabricantes':
+      modal = this.modalCtrl.create(ModalFabricantesPage);
+        break;
+      case 'cliente':
+      modal = this.modalCtrl.create(ModalClientesPage);
+        break;
+    }
+     
+    modal.present();
+  }
 
 }
