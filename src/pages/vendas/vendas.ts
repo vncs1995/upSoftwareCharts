b19c9d43ec1aea7c91786d1285f856eea5577102
Viniciu Viana dos Santos <vinicius.viana@accenture.com>
2018-06-23 01:54:18 -0300

    [WIP] - Os dados estão sendo mostrados dinamicamente, falta consumir o serviço, formatar os campos para money e alterar as cores para mais amigáveis

--- src/pages/vendas/vendas.ts
+++ src/pages/vendas/vendas.ts
@@ -7,8 +7,13 @@ import { NavController } from 'ionic-angular';
 })
 export class VendasPage {
 
+  public dataReceived : any;
   constructor(public navCtrl: NavController) {
+    this.getData();
+  }
 
+  getData() {
+    this.dataReceived = { "VendaPesoCusto": [ { "SMALL_BOX_1_CAPTION": "Faturamento Mensal", "SMALL_BOX_1": "17510", "SMALL_BOX_2_CAPTION": "Faturamento - Projeção do Mês", "SMALL_BOX_2": "23877", "SMALL_BOX_3_CAPTION": "Clientes Positivados", "SMALL_BOX_3": "8", "SMALL_BOX_4_CAPTION": "Produtos Positivados", "SMALL_BOX_4": "12", "PROGRESSBAR_1_CAPTION": "Pedidos Abertos", "PROGRESSBAR_1": "0", "PROGRESSBAR_2_CAPTION": "Pedidos Cancelados", "PROGRESSBAR_2": "0", "PROGRESSBAR_3_CAPTION": "Pedidos Faturados", "PROGRESSBAR_3": "5000" } ] }
   }
 
 }
